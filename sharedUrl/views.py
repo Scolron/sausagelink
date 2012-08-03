92efd05905c6c771830b6525a8fc51079e33d413
Scolron <coltonkinstley@gmail.com>
2012-08-03 06:47:36 -0500

    Bootstraped the shit out of this site

--- sharedUrl/views.py
+++ sharedUrl/views.py
@@ -1,13 +1,15 @@
 # Create your views here.
 from models import SharedSite
-from django.shortcuts import render_to_response, get_list_or_404, get_object_or_404
+from django.shortcuts import render_to_response, get_list_or_404, get_object_or_404, render
 from django.http import HttpRequest
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
+from django.views.decorators.csrf import csrf_protect
+from django.core.context_processors import csrf
 
 
 
-
+@csrf_protect
 def showGroupHarvard(request):
     i = request.META['REMOTE_ADDR']
     url_list = SharedSite.objects.all().filter(ip=i).order_by('-pk')[:30]
@@ -16,7 +18,7 @@ def showGroupHarvard(request):
         bookmark = request.GET['name']
     except(Exception):
         bookmark = ''
-    return render_to_response('harvard_page_list.html',{'url_list':url_list,'bookmark':bookmark})
+    return render(request,'harvard_page_list.html',{'url_list':url_list,'bookmark':bookmark})
 
 def showGroupCorkboard(request):
     i = request.META['REMOTE_ADDR']
@@ -29,17 +31,43 @@ def showGroupCorkboard(request):
     return render_to_response('corkboard_page_list.html',{'url_list':url_list,'bookmark':bookmark})
 
 def add(request):
-    title1 = request.GET['title']
-    usr = request.GET['user']
-    url1 = request.GET['url'] 
-    addr = request.META['REMOTE_ADDR']
-    if title1:
-        new_entry = SharedSite(title=title1, url=url1, user=usr, ip=addr)
-        new_entry.save()
-    else:
-        new_entry = SharedSite(title=url1, url=url1, user=usr, ip=addr)
-        new_entry.save()
     return render_to_response('success.html')
 
+@csrf_protect
 def confirm(request):
-    return render_to_response('confirm.html')
+    if request.method == 'POST':
+        title = request.POST['title']
+        usr = request.POST['user']
+        url = request.POST['url']
+        addr = request.META['REMOTE_ADDR']
+        newEntry = SharedSite(title=title, url=url, user=usr, ip = addr)
+        newEntry.save()
+        return render_to_response('success.html')
+    else:
+        c = {}
+        c['title'] = request.GET['title']
+        c['user'] = request.GET['user']
+        c['url'] = request.GET['url']
+        return render(request,'confirm.html',c)
+    
+def home(request):
+    i = request.META['REMOTE_ADDR']
+    url_list = SharedSite.objects.all().filter(ip=i).order_by('-pk')[:30]
+    try:
+        bookmark = request.GET['name']
+    except(Exception):
+        bookmark = ''
+    return render(request,'sidebar.html',{'url_list':url_list,'bookmark':bookmark})
+
+
+
+
+
+
+
+
+
+
+
+
+
